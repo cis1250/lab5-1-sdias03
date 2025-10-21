5d1c3052cd170777667e8139e5408e7ded5b7c83
Sheldon Dias <sdias03@uoguelph.ca>
2025-10-21 10:53:29 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
+print("Fibonacci! scuzi, babbada boopi? babbada boopi? bibadda boopada babada babada!")
+
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
